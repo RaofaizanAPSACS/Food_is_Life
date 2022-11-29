ee95f1c2cee203d8c046efab96b979d9a6ffdcec
RaofaizanAPSACS <raofaizan123321@gmail.com>
2022-11-29 22:45:45 +0500

    Added the encryption and decryption class

--- FoodisLifeProjectFolder/src/main/java/com/project/fooisLife/repository/register/NgoRegisterRepository.java
+++ FoodisLifeProjectFolder/src/main/java/com/project/fooisLife/repository/register/NgoRegisterRepository.java
@@ -6,17 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.project.fooisLife.config.EncryptDecrypt;
 import com.project.fooisLife.entity.Admin;
 
 @Repository
 public class NgoRegisterRepository {
 	@Autowired
 	private DataSource dataSource;
-	
+
 	public void registerNGOInfo(Admin admin, String name, String address, String city, String state, String phone,
 			String email, String hours) {
-		String sqlQuery = "call RegisterNGO('" + name +"', '"+address+"', '"+city+"', '"+state+"', '"+
-			 	phone+"', '"+email+"', '"+hours+"', '"+admin.getUsername()+"' , '"+admin.getPassword()+"', '"+admin.getEmail()+"', '"+ admin.getPhone()+"')";
+		
+		EncryptDecrypt encrypt = new EncryptDecrypt();
+		
+		String sqlQuery = "call RegisterNGO('" + name + "', '" + address + "', '" + city + "', '" + state + "', '"
+				+ phone + "', '" + email + "', '" + hours + "', '" + admin.getUsername() + "' , '"
+				+ encrypt.encrypt(admin.getPassword(), "FAST") + "', '" + admin.getEmail() + "', '" + admin.getPhone()
+				+ "')";
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 		jdbcTemplate.execute(sqlQuery);
 	}
